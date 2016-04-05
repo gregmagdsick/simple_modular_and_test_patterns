e8a7dfe0d646cc7b5c40f5eb658167f6e5fe962b
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-05 09:37:56 -0700

    fixed first test

--- Greg_day_1_assignment/test/greeter_test.js
+++ Greg_day_1_assignment/test/greeter_test.js
@@ -1,3 +1,9 @@
-const Greeter = requre(__dirname + '/../lib/greeter');
+const Greeter = require(__dirname + '/../lib/greeter');
+const expect = require('chai').expect;
+//const greeter = new Greeter('My New Best Friend');
 
-const greeter = new Greeter('My New Best Friend');
+describe('The Greeter Test', () => {
+  it('says hello to you', () => {
+    expect(Greeter('My New Best Friend')).to.eql('Hello My New Best Friend');
+  });
+});
